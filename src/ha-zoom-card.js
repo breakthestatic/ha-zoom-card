e371fb96791332a4a5410b5bdd80673669e999dd
breakthestatic <breakthestatic@gmail.com>
2022-05-04 13:44:49 -0700

    Basic setup

--- src/ha-zoom-card.js
+++ src/ha-zoom-card.js
@@ -1,54 +1,166 @@
-import Panzoom from "@panzoom/panzoom";
+import Panzoom from "@panzoom/panzoom"
 
 class ZoomCard extends HTMLElement {
-  // Whenever the state changes, a new `hass` object is set. Use this to
-  // update your content.
-  set hass(hass) {
-    // Initialize the content if it's not there yet.
-    if (!this.content) {
-      this.innerHTML = `
-        <ha-card header="Example-card3">
-          <div class="card-content"></div>
-        </ha-card>
-      `;
-      this.content = this.querySelector("div");
-    }
-
-    const entityId = this.config.entity;
-    const state = hass.states[entityId];
-    const stateStr = state ? state.state : "unavailable";
-
-    this.content.innerHTML = `
-      The state of ${entityId} is ${stateStr}!
-      <br><br>
-      <img src="http://via.placeholder.com/350x150">
-    `;
+  constructor() {
+    super()
   }
 
   connectedCallback() {
-    if (this.content) {
-      const panzoom = Panzoom(this.content, { contain: "outside" });
-      this.content.parentElement.addEventListener(
-        "wheel",
-        panzoom.zoomWithWheel
-      );
+    if (this.card) {
+      this.panzoom = Panzoom(this.cardWrapper, {contain: "outside"})
+      this.cardWrapper.addEventListener("wheel", this.panzoom.zoomWithWheel)
     }
   }
 
-  // The user supplied configuration. Throw an exception and Home Assistant
-  // will render an error card.
   setConfig(config) {
-    if (!config.entity) {
-      throw new Error("You need to define an entity");
+    this._cardSize = {}
+    this._cardSize.promise = new Promise(
+      (resolve) => (this._cardSize.resolve = resolve)
+    )
+
+    if (!config || !config.card) {
+      throw new Error("Card config incorrect")
     }
-    this.config = config;
+    this.config = config
+    this.card = null
+    this.renderCard()
   }
 
-  // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
-  getCardSize() {
-    return 1;
+  async renderCard() {
+    if (window.loadCardHelpers) {
+      this.helpers = await window.loadCardHelpers()
+    }
+
+    this.card = await this.createCardElement(this.config.card)
+    this.cardWrapper = document.createElement("div")
+
+    // Style cards
+    if (this.card.updateComplete) {
+      this.card.updateComplete.then(() => this.styleCard(this.card))
+    } else {
+      this.styleCard(this.card)
+    }
+
+    // Create the card
+    const root = document.createElement("ha-card")
+    this.cardWrapper.appendChild(this.card)
+
+    root.appendChild(this.cardWrapper)
+    while (this.hasChildNodes()) {
+      this.removeChild(this.lastChild)
+    }
+    this.appendChild(root)
+
+    // Calculate card size
+    this._cardSize.resolve()
+  }
+
+  async createCardElement(cardConfig) {
+    const createError = (error, origConfig) => {
+      return createThing("hui-error-card", {
+        type: "error",
+        error,
+        origConfig,
+      })
+    }
+
+    const createThing = (tag, config) => {
+      if (this.helpers) {
+        return this.helpers.createCardElement(config)
+      }
+
+      const element = document.createElement(tag)
+      try {
+        element.setConfig(config)
+      } catch (err) {
+        console.error(tag, err)
+        return createError(err.message, config)
+      }
+      return element
+    }
+
+    let tag = cardConfig.type
+    if (tag.startsWith("custom:")) {
+      tag = tag.substr("custom:".length)
+    } else {
+      tag = `hui-${tag}-card`
+    }
+
+    const element = createThing(tag, cardConfig)
+    element.hass = this._hass
+    element.addEventListener(
+      "ll-rebuild",
+      (ev) => {
+        ev.stopPropagation()
+        this.createCardElement(cardConfig).then(() => {
+          this.renderCard()
+        })
+      },
+      {once: true}
+    )
+    return element
+  }
+
+  set hass(hass) {
+    this._hass = hass
+    if (this.card) {
+      this.card.hass = hass
+    }
+  }
+
+  styleCard(element) {
+    if (element.shadowRoot) {
+      if (element.shadowRoot.querySelector("ha-card")) {
+        const ele = element.shadowRoot.querySelector("ha-card")
+        ele.style.boxShadow = "none"
+        ele.style.borderRadius = "0"
+      } else {
+        let searchEles = element.shadowRoot.getElementById("root")
+        if (!searchEles) {
+          searchEles = element.shadowRoot.getElementById("card")
+        }
+        if (!searchEles) return
+        searchEles = searchEles.childNodes
+        for (let i = 0; i < searchEles.length; i++) {
+          if (searchEles[i].style) {
+            searchEles[i].style.margin = "0px"
+          }
+          this.styleCard(searchEles[i])
+        }
+      }
+    } else {
+      if (
+        typeof element.querySelector === "function" &&
+        element.querySelector("ha-card")
+      ) {
+        const ele = element.querySelector("ha-card")
+        ele.style.boxShadow = "none"
+        ele.style.borderRadius = "0"
+      }
+      const searchEles = element.childNodes
+      for (let i = 0; i < searchEles.length; i++) {
+        if (searchEles[i] && searchEles[i].style) {
+          searchEles[i].style.margin = "0px"
+        }
+        this.styleCard(searchEles[i])
+      }
+    }
+  }
+
+  _computeCardSize(card) {
+    if (typeof card.getCardSize === "function") {
+      return card.getCardSize()
+    }
+    return customElements
+      .whenDefined(card.localName)
+      .then(() => this._computeCardSize(card))
+      .catch(() => 1)
+  }
+
+  async getCardSize() {
+    await this._cardSize.promise
+    return await this._computeCardSize(this.card)
   }
 }
 
-customElements.define("zoom-card", ZoomCard);
+customElements.define("zoom-card", ZoomCard)

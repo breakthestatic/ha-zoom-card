56d9d87225c71e2f2f8e3fcc9746405b8d68abb2
breakthestatic <breakthestatic@gmail.com>
2022-09-27 14:24:32 -0700

    Allow more custom zoom options

--- src/ha-zoom-card.js
+++ src/ha-zoom-card.js
@@ -8,145 +8,135 @@ class ZoomCard extends HTMLElement {
     this.attachShadow({mode: "open"})
   }
 
+  createThing(tag, config) {
+    const element = document.createElement(tag)
+
+    try {
+      element.setConfig(config)
+    } catch (err) {
+      console.error(tag, err)
+      return this.createError(err.message, config)
+    }
+    return element
+  }
+
+  createError(error, config) {
+    return this.createThing("hui-error-card", {type: "error", error, config})
+  }
+
+  fireEvent(event, detail, entity = null) {
+    event = new Event(event, {
+      bubbles: true,
+      cancelable: false,
+      composed: true,
+    })
+
+    event.detail = detail || {}
+
+    if (entity) {
+      entity.dispatchEvent(event)
+    } else {
+      document
+        .querySelector("home-assistant")
+        .shadowRoot.querySelector("home-assistant-main")
+        .shadowRoot.querySelector("app-drawer-layout partial-panel-resolver")
+        .shadowRoot.querySelector("ha-panel-lovelace")
+        .shadowRoot.querySelector("hui-root")
+        .shadowRoot.querySelector("ha-app-layout #view")
+        .firstElementChild.dispatchEvent(event)
+    }
+  }
+
   setConfig(config) {
-    if (!config || !config.cards || !Array.isArray(config.cards)) {
+    if (!config || !config.card) {
       throw new Error("Card config incorrect")
     }
 
     this.config = config
-    this.style.boxShadow =
-      "var(--ha-card-box-shadow, 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2))"
-    this.style.borderRadius = "var(--ha-card-border-radius, 2px)"
-    this.style.background = "var(--paper-card-background-color)"
-    this.style.display = "block"
 
     const root = this.shadowRoot
+
+    // Remove all children
     while (root.hasChildNodes()) {
       root.removeChild(root.lastChild)
     }
 
     // Wrap main card
     const wrapper = document.createElement("div")
-    this.zoomable = document.createElement("div")
+    this.zoomable = document.createElement("ha-card")
     wrapper.setAttribute("style", "position:relative")
     this.zoomable.appendChild(wrapper)
     root.appendChild(this.zoomable)
 
-    this._refCards = []
+    this._refCard = null
 
-    const _createThing = (tag, config) => {
-      const element = document.createElement(tag)
+    const {type} = config.card
+    const tag = type.startsWith("custom:")
+      ? type.substr("custom:".length)
+      : `hui-${type}-card`
 
-      try {
-        element.setConfig(config)
-      } catch (err) {
-        console.error(tag, err)
-        return _createError(err.message, config)
-      }
-      return element
-    }
+    if (customElements.get(tag)) {
+      const element = this.createThing(tag, config.card)
 
-    const _createError = (error, config) =>
-      _createThing("hui-error-card", {
-        type: "error",
-        error,
-        config,
+      wrapper.appendChild(element)
+
+      this._refCard = element
+    } else {
+      // If element doesn't exist (yet) create an error
+      const element = this.createError(
+        `Custom element doesn't exist: ${tag}.`,
+        config.card
+      )
+      element.style.display = "None"
+
+      const time = setTimeout(() => {
+        element.style.display = ""
+      }, 2000)
+
+      // Remove error if element is defined later
+      customElements.whenDefined(tag).then(() => {
+        clearTimeout(time)
+        this.fireEvent("ll-rebuild", {}, element)
       })
 
-    const _fireEvent = (ev, detail, entity = null) => {
-      ev = new Event(ev, {
-        bubbles: true,
-        cancelable: false,
-        composed: true,
-      })
-
-      ev.detail = detail || {}
-
-      if (entity) {
-        entity.dispatchEvent(ev)
-      } else {
-        document
-          .querySelector("home-assistant")
-          .shadowRoot.querySelector("home-assistant-main")
-          .shadowRoot.querySelector("app-drawer-layout partial-panel-resolver")
-          .shadowRoot.querySelector("ha-panel-lovelace")
-          .shadowRoot.querySelector("hui-root")
-          .shadowRoot.querySelector("ha-app-layout #view")
-          .firstElementChild.dispatchEvent(ev)
-      }
+      root.appendChild(element)
+      this._refCard = element
     }
-
-    config.cards.forEach((item) => {
-      let tag = item.type
-
-      if (tag.startsWith("divider")) {
-        tag = `hui-divider-row`
-      } else if (tag.startsWith("custom:")) {
-        tag = tag.substr("custom:".length)
-      } else {
-        tag = `hui-${tag}-card`
-      }
-
-      if (customElements.get(tag)) {
-        const element = _createThing(tag, item)
-
-        wrapper.appendChild(element)
-
-        this._refCards.push(element)
-      } else {
-        // If element doesn't exist (yet) create an error
-        const element = _createError(
-          `Custom element doesn't exist: ${tag}.`,
-          item
-        )
-        element.style.display = "None"
-
-        const time = setTimeout(() => {
-          element.style.display = ""
-        }, 2000)
-
-        // Remove error if element is defined later
-        customElements.whenDefined(tag).then(() => {
-          clearTimeout(time)
-          _fireEvent("ll-rebuild", {}, element)
-        })
-
-        root.appendChild(element)
-        this._refCards.push(element)
-      }
-    })
   }
 
   set hass(hass) {
-    if (this._refCards) {
-      this._refCards.forEach((card) => {
-        card.hass = hass
-      })
+    if (this._refCard) {
+      this._refCard.hass = hass
     }
   }
 
   connectedCallback() {
-    this._refCards.forEach((element) => {
-      const fn = () => {
-        this._card(element)
-      }
+    const fn = () => {
+      this._card(this._refCard)
+    }
 
-      if (element.updateComplete) {
-        element.updateComplete.then(fn)
-      } else {
-        fn()
-      }
-    })
+    if (this._refCard.updateComplete) {
+      this._refCard.updateComplete.then(fn)
+    } else {
+      fn()
+    }
 
     if (!this.panzoom) {
+      const {zoomOptions, tapTimeout} = this.config
+      const {step = 1, maxScale = 6, ...config} = zoomOptions || {}
       const zoomTarget = this.zoomable.firstElementChild
       this.panzoom = Panzoom(zoomTarget, {
         contain: "outside",
-        step: this.config.step,
-        maxScale: this.config.maxScale,
+        step,
+        maxScale,
+        panOnlyWhenZoomed: true,
+        ...config,
       })
       zoomTarget.addEventListener("wheel", this.panzoom.zoomWithWheel)
-      zoomTarget.addEventListener("touchend", doubleTap(this.handleZoom()))
+      zoomTarget.addEventListener(
+        "touchend",
+        doubleTap(this.handleZoom(), tapTimeout)
+      )
       zoomTarget.addEventListener("dblclick", this.handleZoom())
     }
   }
@@ -177,6 +167,7 @@ class ZoomCard extends HTMLElement {
         }
       } else {
         element.shadowRoot.querySelector("ha-card").style.boxShadow = "none"
+        element.shadowRoot.querySelector("ha-card").style.borderRadius = "0px"
       }
     } else {
       if (
@@ -184,6 +175,7 @@ class ZoomCard extends HTMLElement {
         element.querySelector("ha-card")
       ) {
         element.querySelector("ha-card").style.boxShadow = "none"
+        element.querySelector("ha-card").style.borderRadius = "0px"
       }
       const searchEles = element.childNodes
       for (let i = 0; i < searchEles.length; i++) {
@@ -196,11 +188,11 @@ class ZoomCard extends HTMLElement {
   }
 
   getCardSize() {
-    let totalSize = 0
-    this._refCards.forEach((element) => {
-      totalSize +=
-        typeof element.getCardSize === "function" ? element.getCardSize() : 1
-    })
+    const totalSize =
+      typeof this._refCard.getCardSize === "function"
+        ? this._refCard.getCardSize()
+        : 1
+
     return totalSize
   }
 }
